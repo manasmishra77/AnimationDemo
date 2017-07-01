d7d5ea776d373d1d127ace8efe5c49e88fb06efe
Manas Mishra <manasmishra@Manas-MacBook-Air.local>
2017-07-01 19:38:25 +0530

    Initial Commit

---
+++ AnimationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Manas Mishra on 01/07/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
